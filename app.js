5d0e981fd3fbb5a1fc1282c7f2b7b55fc9a74c2e
Kashif Raza <k.raza1932000@gmail.com>
2023-07-03 12:55:42 +0530

    update

--- app.js
+++ app.js
@@ -12,30 +12,3 @@
     })
 })();
 
-$(document).ready(function() {
-    $('#contact-form').submit(function(event) {
-      event.preventDefault();
-      var name = $('#name').val();
-      var email = $('#email').val();
-      var message = $('#message').val();
-      $.ajax({
-        type: 'POST',
-        url: 'https://api.mailgun.net/v3/sandboxdb227e2c5c634783ad37592f95706eb1.mailgun.org/messages',
-        headers: {
-          'Authorization': 'Basic ' + btoa('api:key-3e89447bb457f88de123ec4435aa6583')
-        },
-        data: {
-          'from': email,
-          'to': 'k.raza1932000@gmail.com',
-          'subject': 'New message from ' + name,
-          'text': message
-        },
-        success: function(response) {
-          alert('Message sent successfully!');
-        },
-        error: function(response) {
-          alert('Error sending message.');
-        }
-      });
-    });
-  });
